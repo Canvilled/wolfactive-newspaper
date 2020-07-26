76bcf96b6e7bb243040b21fd8855e1456cb8f0d8
DatTH <57146764+hotandat260399@users.noreply.github.com>
2020-07-26 23:09:53 +0700

    update again .

--- core/assets/js/convert/main/_my-script.js
+++ core/assets/js/convert/main/_my-script.js
@@ -1,45 +1,47 @@
 "use strict";
 
-var parent = document.querySelector('.popular__post-single-container'),
-    items = parent.querySelectorAll('.popupar__post-single-item'),
-    loadMoreBtn = document.querySelector('#load-more-post'),
-    closePostBtn = document.querySelector('#close-post'),
-    maxItems = 9;
-[].forEach.call(items, function (item, idx) {
-  if (idx > maxItems / 3 - 1) {
-    item.classList.add('visually-hidden');
-  }
-});
-closePostBtn.classList.add('d--none');
-
-loadMoreBtn.onclick = function () {
-  var hiddenPost = document.querySelectorAll('.visually-hidden');
-  [].forEach.call(hiddenPost, function (item, idx) {
-    if (idx < maxItems / 3) {
-      item.classList.remove('visually-hidden');
-      item.classList.add('visually-unset');
-    }
-
-    if (document.querySelectorAll('.visually-hidden').length === 0) {
-      var hiddendLoadMore = document.querySelector('#load-more-post');
-      hiddendLoadMore.classList.add('d--none');
-      closePostBtn.classList.remove('d--none');
-    }
-  });
-};
-
-closePostBtn.onclick = function () {
-  var hiddenPost = document.querySelectorAll('.visually-unset');
-  [].forEach.call(hiddenPost, function (item, idx) {
-    if (hiddenPost.length > 0) {
-      item.classList.remove('visually-unset');
+if (document.querySelector('.popular__post-single')) {
+  var parent = document.querySelector('.popular__post-single-container'),
+      items = parent.querySelectorAll('.popupar__post-single-item'),
+      loadMoreBtn = document.querySelector('#load-more-post'),
+      closePostBtn = document.querySelector('#close-post'),
+      maxItems = 9;
+  [].forEach.call(items, function (item, idx) {
+    if (idx > maxItems / 3 - 1) {
       item.classList.add('visually-hidden');
     }
-
-    if (document.querySelectorAll('.visually-unset').length === 0) {
-      var hiddendClosePost = document.querySelector('#close-post');
-      hiddendClosePost.classList.add('d--none');
-      loadMoreBtn.classList.remove('d--none');
-    }
   });
-};
+  closePostBtn.classList.add('d--none');
+
+  loadMoreBtn.onclick = function () {
+    var hiddenPost = document.querySelectorAll('.visually-hidden');
+    [].forEach.call(hiddenPost, function (item, idx) {
+      if (idx < maxItems / 3) {
+        item.classList.remove('visually-hidden');
+        item.classList.add('visually-unset');
+      }
+
+      if (document.querySelectorAll('.visually-hidden').length === 0) {
+        var hiddendLoadMore = document.querySelector('#load-more-post');
+        hiddendLoadMore.classList.add('d--none');
+        closePostBtn.classList.remove('d--none');
+      }
+    });
+  };
+
+  closePostBtn.onclick = function () {
+    var hiddenPost = document.querySelectorAll('.visually-unset');
+    [].forEach.call(hiddenPost, function (item, idx) {
+      if (hiddenPost.length > 0) {
+        item.classList.remove('visually-unset');
+        item.classList.add('visually-hidden');
+      }
+
+      if (document.querySelectorAll('.visually-unset').length === 0) {
+        var hiddendClosePost = document.querySelector('#close-post');
+        hiddendClosePost.classList.add('d--none');
+        loadMoreBtn.classList.remove('d--none');
+      }
+    });
+  };
+}
--- core/assets/js/core/main/_my-script.js
+++ core/assets/js/core/main/_my-script.js
@@ -1,45 +1,45 @@
-var parent = document.querySelector('.popular__post-single-container'),
-    items  = parent.querySelectorAll('.popupar__post-single-item'),
-    loadMoreBtn =  document.querySelector('#load-more-post'),
-    closePostBtn = document.querySelector('#close-post'),
-    maxItems = 9;
 
+if(document.querySelector('.popular__post-single')){
+  let parent = document.querySelector('.popular__post-single-container'),
+      items  = parent.querySelectorAll('.popupar__post-single-item'),
+      loadMoreBtn =  document.querySelector('#load-more-post'),
+      closePostBtn = document.querySelector('#close-post'),
+      maxItems = 9;
+      [].forEach.call(items, function(item, idx){
+          if (idx > maxItems/3 - 1) {
+              item.classList.add('visually-hidden');
+          }
+      });
 
+      closePostBtn.classList.add('d--none');
 
-[].forEach.call(items, function(item, idx){
-    if (idx > maxItems/3 - 1) {
-        item.classList.add('visually-hidden');
-    }
-});
-
-closePostBtn.classList.add('d--none');
-
-loadMoreBtn.onclick = () => {
-  let hiddenPost = document.querySelectorAll('.visually-hidden');
-  [].forEach.call(hiddenPost, function(item, idx){
-      if (idx < maxItems/3) {
-          item.classList.remove('visually-hidden');
-          item.classList.add('visually-unset');
+      loadMoreBtn.onclick = () => {
+        let hiddenPost = document.querySelectorAll('.visually-hidden');
+        [].forEach.call(hiddenPost, function(item, idx){
+            if (idx < maxItems/3) {
+                item.classList.remove('visually-hidden');
+                item.classList.add('visually-unset');
+            }
+            if ( document.querySelectorAll('.visually-hidden').length === 0) {
+                let hiddendLoadMore = document.querySelector('#load-more-post');
+                hiddendLoadMore.classList.add('d--none');
+                closePostBtn.classList.remove('d--none');
+            }
+        });
       }
-      if ( document.querySelectorAll('.visually-hidden').length === 0) {
-          let hiddendLoadMore = document.querySelector('#load-more-post');
-          hiddendLoadMore.classList.add('d--none');
-          closePostBtn.classList.remove('d--none');
-      }
-  });
-}
 
-closePostBtn.onclick = () => {
-  let hiddenPost = document.querySelectorAll('.visually-unset');
-  [].forEach.call(hiddenPost, function(item, idx){
-      if (hiddenPost.length > 0) {
-          item.classList.remove('visually-unset');
-          item.classList.add('visually-hidden');
+      closePostBtn.onclick = () => {
+        let hiddenPost = document.querySelectorAll('.visually-unset');
+        [].forEach.call(hiddenPost, function(item, idx){
+            if (hiddenPost.length > 0) {
+                item.classList.remove('visually-unset');
+                item.classList.add('visually-hidden');
+            }
+            if ( document.querySelectorAll('.visually-unset').length === 0) {
+                let hiddendClosePost = document.querySelector('#close-post');
+                hiddendClosePost.classList.add('d--none');
+                loadMoreBtn.classList.remove('d--none');
+            }
+        });
       }
-      if ( document.querySelectorAll('.visually-unset').length === 0) {
-          let hiddendClosePost = document.querySelector('#close-post');
-          hiddendClosePost.classList.add('d--none');
-          loadMoreBtn.classList.remove('d--none');
-      }
-  });
 }
